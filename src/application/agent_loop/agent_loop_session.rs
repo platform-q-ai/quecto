bc7847af980f5b7b8e49a3ae98d066a87dd50bd1
SQ <37052934+quigles1977@users.noreply.github.com>
2026-06-22 23:48:44 +0100

    Fix recall session key rotation

--- src/application/agent_loop/agent_loop_session.rs
+++ src/application/agent_loop/agent_loop_session.rs
@@ -3,6 +3,7 @@ use super::AgentLoopImpl;
 impl AgentLoopImpl {
     /// Switch the session key used for provider session IDs and spill recall.
     pub fn set_session_key(&mut self, session_key: String) {
-        self.session_key = session_key;
+        self.session_key = session_key.clone();
+        self.tool_registry.set_session_key(&session_key);
     }
 }
